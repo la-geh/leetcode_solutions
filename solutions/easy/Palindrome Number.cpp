24adbd97ce8b6a90e3565736b00debc75ec474da
la-geh <laureline.gehrmann@gmx.ch>
2026-07-31 17:39:36 +0200

    Add solution for Palindrome Number

--- solutions/easy/Palindrome Number.cpp
+++ solutions/easy/Palindrome Number.cpp
@@ -3,16 +3,4 @@
             // Language: C++
             // Link: https://leetcode.com/problems/palindrome-number/
 
-class Solution {
-public:
-    bool isPalindrome(int x) {
-    if (x < 0) return false;
-    long reversed = 0;
-    long original = x;
-    while (x != 0) {
-        reversed = reversed * 10 + x % 10;
-        x /= 10;
-    return reversed == original;
-        }
-    }
-};
+c

4f91c8fd1e46fed2ff380779bcb16d21cdf82036
la-geh <laureline.gehrmann@gmx.ch>
2026-07-31 17:14:49 +0200

    Add solution for Add Two Numbers

---
+++ solutions/medium/Add Two Numbers.cpp
@@ -0,0 +1,23 @@
+// Title: Add Two Numbers
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/add-two-numbers/
+
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+        int carry = 0;
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int x = (l1 != nullptr) ? l1 -> val : 0;
+            int y = (l2 != nullptr) ? l2 -> val:0; 
+            int sum = x + y + carry;
+            carry = sum / 10;
+            tail -> next = new ListNode(sum%10);
+            tail = tail->next;
+            if (l1 != nullptr) l1 = l1-> next;
+            if (l2 != nullptr) l2 = l2->next;
+        }
+        return dummy -> next;
+    }
+};
